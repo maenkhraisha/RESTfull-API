ed849d2b8c9bf96e0073e58310e9e1d2646ca189
maen <maenkh@yahoo.com>
2022-05-20 03:03:08 +0300

    add users CRUD operation

--- index.js
+++ index.js
@@ -10,8 +10,11 @@ const http = require('http');
 const https = require('https');
 const URL = require('url');
 const StringDecoder = require('string_decoder').StringDecoder;
-const config = require('./config.js');
+const config = require('./lib/config.js');
 const fs = require('fs');
+const _data = require('./lib/data');
+const handlers = require('./lib/handlers');
+const helpers = require('./lib/helpers');
 
 // instantiate the HTTP server
 const httpServer = http.createServer(function(req,res){
@@ -74,7 +77,7 @@ const unifiedServer = function(req,res){
       'queryStringObject' : queryStringObject,
       'method' : method,
       'headers' : headers,
-      'payload' : buffer
+      'payload' : helpers.parseJsonToObject(buffer)
     };
 
     //router the request to the handler specified in the router
@@ -100,20 +103,8 @@ const unifiedServer = function(req,res){
   });
 }
 
-//define handler
-const handlers = {};
-
-//ping handler
-handlers.ping = function(data,callback){
-  callback(200);
-};
-
-//not found handler
-handlers.notFound = function(data,callback){
-  callback(404);
-}
-
 //define a request router
 const router = {
-  'ping' : handlers.ping
+  'ping' : handlers.ping,
+  'users' : handlers.users
 };
